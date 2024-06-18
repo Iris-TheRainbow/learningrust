31e1ee229237a25d30053a0563adb8a4081d3688
iris <snazzsinclair@gmail.com>
2024-06-17 23:50:17 -0500

    try a new buffer and timeout on the read?

--- src/main.rs
+++ src/main.rs
@@ -10,9 +10,8 @@ fn main() {
     let mut port = serialport::new("/dev/ttyUSB0", 460800)
     .timeout(Duration::from_millis(10))
     .open().expect("Failed to open port");
-    let output = "This is a test. This is only a test.".as_bytes();
+    let output = "DK".as_bytes();
     port.write(output).expect("Write failed!");
-    let mut serial_buf: Vec<u8> = vec![0; 32];
+    let mut serial_buf: Vec<u8> = vec![20; 64];
     port.read(serial_buf.as_mut_slice()).expect("Found no data!");
-
 }
